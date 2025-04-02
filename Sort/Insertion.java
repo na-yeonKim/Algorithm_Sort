f2eb87f83860b1d5460b6bc09b03c5d1c9f8d74a
na-yeonKim <165547330+na-yeonKim@users.noreply.github.com>
2025-04-02 15:47:36 +0900

    Update Insertion.java

--- Sort/Insertion.java
+++ Sort/Insertion.java
@@ -12,7 +12,7 @@ public class Insertion extends AbstractSort{
 
     public static void main(String[] args) {
         Integer[] a = {10, 4, 5, 2, 1, 8, 3, 6};
-        Selection.sort(a);
-        Selection.show(a);
+        Insertion.sort(a);
+        Insertion.show(a);
     }
 }
